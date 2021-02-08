959f24b2696ba01275c163cab82816873c6072ef
dolinsky <martin.dolinsky@akademiasovy.sk>
2021-02-08 21:24:08 +0100

    Added admin guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,29 +18,30 @@ import {NotFoundComponent} from './not-found/not-found.component';
 import {MovieDetailsComponent} from './movie-details/movie-details.component';
 import {TvseriesDetailsComponent} from './tvseries-details/tvseries-details.component';
 import {AuthGuard} from './guards/auth.guard';
+import {AdminGuard} from './guards/admin.guard';
 
 const routes: Routes = [
     {
-        path: 'admin', component: BoardAdminComponent, canActivate: [AuthGuard],
+        path: 'admin', component: BoardAdminComponent, canActivate: [AdminGuard],
         children: [
             {path: 'movies', component: MoviesTableComponent},
             {path: 'tvseries', component: TvseriesTableComponent},
             {path: 'users', component: UsersTableComponent},
         ]
     },
-    {path: 'home', component: HomeComponent},
+    {path: 'home', component: HomeComponent, canActivate: [AuthGuard, AdminGuard]},
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'register', component: RegisterComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'profile', component: ProfileComponent},
-    {path: 'movies', component: MoviesComponent},
-    {path: 'tvseries', component: TvseriesComponent},
-    {path: 'edit/movies/:id', component: EditMovieComponent, canActivate: [AuthGuard]},
-    {path: 'edit/tvseries/:id', component: EditTvseriesComponent, canActivate: [AuthGuard]},
-    {path: 'create/movie', component: CreateMovieComponent, canActivate: [AuthGuard]},
-    {path: 'create/tvseries', component: CreateTvseriesComponent, canActivate: [AuthGuard]},
-    {path: 'movies/:id', component: MovieDetailsComponent},
-    {path: 'tvseries/:id', component: TvseriesDetailsComponent},
+    {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard, AdminGuard]},
+    {path: 'movies', component: MoviesComponent, canActivate: [AuthGuard, AdminGuard]},
+    {path: 'tvseries', component: TvseriesComponent, canActivate: [AuthGuard, AdminGuard]},
+    {path: 'edit/movies/:id', component: EditMovieComponent, canActivate: [AdminGuard]},
+    {path: 'edit/tvseries/:id', component: EditTvseriesComponent, canActivate: [AdminGuard]},
+    {path: 'create/movie', component: CreateMovieComponent, canActivate: [AdminGuard]},
+    {path: 'create/tvseries', component: CreateTvseriesComponent, canActivate: [AdminGuard]},
+    {path: 'movies/:id', component: MovieDetailsComponent, canActivate: [AuthGuard, AdminGuard]},
+    {path: 'tvseries/:id', component: TvseriesDetailsComponent, canActivate: [AuthGuard, AdminGuard]},
     {path: '**', component: NotFoundComponent}
 ];
 
